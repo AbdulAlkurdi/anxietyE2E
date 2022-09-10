6e3da98ee91e1f683b2684f482171085d5136702
AbdulAlkurdi <abdul.alkurdi@gmail.com>
2022-09-10 15:59:10 -0500

    push stressnet scripts

---
+++ tuningSTRES3of3.sh
@@ -0,0 +1,10 @@
+# This script was created because of probable memory leakage in version of tensorflow/keras used in this project
+for clas in stresnetM; do
+  for dataset in wesad; do
+    for max_eval in $(seq 1 10); do
+      for i_fold in 04; do
+        python tune_one.py "$1" "$dataset"_fold_05_"$i_fold" $clas "$max_eval"
+      done
+    done
+  done
+done
