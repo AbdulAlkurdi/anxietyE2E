bef5824f4fd4f40a1fb7f917974262e9da824a4c
Abdul Alkurdi <alkurdi2@illinois.edu>
2023-11-24 12:30:02 -0600

    morning push

--- batch_tuning.sh
+++ batch_tuning.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-for clas in cnnM resnetM; do
+#for clas in cnnM resnetM; do
+#    for max_eval in $(seq 1 10); do
+#        sbatch job.sbatch $clas $max_eval
+#    done;
+#done;
 
-    for max_eval in $(seq 1 10); do
-
-        sbatch job.sbatch $clas $max_eval
-    done;
-done;
-
-
-for clas in  fcnM cnnM resnetM mcdcnnM  inceptionM stresnetM encoderM  mlpM mlpLstmM cnnLstmM; do 
-  for max_eval in $(seq 1 10); do 
-    for i_fold in 00 01 02 03 04; do
+for max_eval in $(seq 1 10); do
+  for  i_fold in 00 01 02 03 04; do
+    for clas in  fcnM cnnM resnetM mcdcnnM  inceptionM stresnetM encoderM  mlpM mlpLstmM cnnLstmM; do 
       sbatch training.sbatch $i_fold $clas $max_eval
     done
   done
--- result_checking.py
+++ result_checking.py
@@ -2,17 +2,22 @@ import os
 import sys
 if __name__ == '__main__':
 
-	_, dataset_name, classifier, max_eval, num_iteration = sys.argv
-
-	data_dir = "results"
-
-	for dataset_fold in range(5):
-		for tune_trial in range(int(max_eval)):
-			for iteration in range(int(num_iteration)):
-				
-				path = f"{data_dir}/{dataset_name}_5fold_{dataset_fold:02d}/tune_{tune_trial:02d}/{classifier}/it_{iteration:02d}"
-				print(path)
-				if (os.path.exists(path)):
-					print(f"Results for {classifier}, {dataset_name}, fold {dataset_fold}, tune {tune_trial}, iteration {iteration} already exists")
-				else:
-					print(f"Results for {classifier}, {dataset_name}, fold {dataset_fold}, tune {tune_trial}, iteration {iteration} missing")
+	#_, dataset_name, classifier, max_eval, num_iteration = sys.argv
+	max_eval = 10
+	num_iteration = 5
+	dataset_name = 'WESAD'
+	data_dir = "results_reduced"
+	for classifier in ['stresnetM' ,'fcnM', 'stresnetM', 
+					'resnetM', 'mcdcnnM', 'cnnM', 'inceptionM',
+					'encoderM', 'mlpM', 'mlpLstmM', 'cnnLstmM']:
+		print(classifier)	
+		for dataset_fold in range(5):
+			for tune_trial in range(int(max_eval)):
+				for iteration in range(int(num_iteration)):
+					
+					path = f"{data_dir}/{dataset_name}_5fold_{dataset_fold:02d}/tune_{tune_trial:02d}/{classifier}/it_{iteration:02d}"
+					#print(path)
+					if (os.path.exists(path)):
+						print(f"Results for {classifier}, {dataset_name}, fold {dataset_fold}, tune {tune_trial}, iteration {iteration} already exists")
+					else:
+						print(f"Results for {classifier}, {dataset_name}, fold {dataset_fold}, tune {tune_trial}, iteration {iteration} missing")
