f2818047afd8993ca9471432064fa8a56f9a6d36
AbdulAlkurdi <abdul.alkurdi@gmail.com>
2023-11-29 00:35:20 -0600

    late night push

--- tuningtwo-2.sh
+++ tuningtwo-2.sh
@@ -1,8 +1,8 @@
 # This script was created because of probable memory leakage in version of tensorflow/keras used in this project
-for clas in cnnM mcdcnnM fcnM mlpM mlpLstmM cnnLstmM InceptionM resnetM encoderM stresnetM; do #cnnLstmM mlpLstmM mlpM encoderM  
+for clas in inceptionM ; do #cnnM mcdcnnM fcnM mlpM mlpLstmM cnnLstmM InceptionM resnetM encoderM stresnetM; do #cnnLstmM mlpLstmM mlpM encoderM  
   for dataset in wesad; do
-    for max_eval in $(seq 1 10); do
-      for i_fold in 00 01 02 03 04; do
+    for i_fold in 04 03 02 01 00; do #max_eval in $(seq 7 10); do
+      for max_eval in $(seq 7 10); do # i_fold in 04 03 02 03 04; do
         python tune_two-2.py "$0" "$dataset"_fold_05_"$i_fold" $clas "$max_eval"
       done
     done
